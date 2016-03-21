c58b66812c20f35089df3832e00447af7ad16a3c
ameer <faraj.ameer@gmail.com>
2016-03-21 13:12:41 +0200

    Added config.js file

---
+++ config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    name: 'Custom UI Test',
+    type: 'staticContent',
+    multi: null,
+    dashUI: true,
+    src: {
+        repo: 'testDelete',
+        owner: 'soajsTestAccount',
+        main: '/index.js'
+    },
+    prerequisites: {
+        cpu: '',
+        memory: ''
+    }
+};
