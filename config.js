165382c8b7fd765b49fc5f972d7a60a12cc0ccfb
elianenassif <nassif.eliane@gmail.com>
2018-08-10 16:02:29 +0300

    testing

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
 'use strict';
-
-module.exports = {
-    name: 'customuitest',
-    type: 'static',
-    dashUI: false,
-    prerequisites: {
-        cpu: '',
-        memory: ''
-    }
-};
---
+++ soa.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    name: 'customuitest',
+    type: 'static',
+    dashUI: false,
+    prerequisites: {
+        cpu: '',
+        memory: ''
+    }
+};
